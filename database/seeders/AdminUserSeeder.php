48529fb9a9aa910bdb606f851f6fa0622baec66c
Vanisa Indiyani <vanisaindriyani30@gmail.com>
2025-08-03 07:04:05 +0700

    First commit

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -14,10 +14,10 @@ class AdminUserSeeder extends Seeder
     public function run(): void
     {
         DB::table('users')->updateOrInsert(
-            ['email' => 'admin@admin.com'],
+            ['email' => 'kt6581387@gmail.com'],
             [
                 'name' => 'Admin',
-                'email' => 'admin@admin.com',
+                'email' => 'kt6581387@gmail.com',
                 'password' => Hash::make('password'),
                 'role' => 'admin',
                 'created_at' => now(),
